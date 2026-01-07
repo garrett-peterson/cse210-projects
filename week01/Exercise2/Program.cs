eb812a42a2b3c587bee60144b9f20044b6279eae
garrett-peterson <garrett.peterson726@gmail.com>
2026-01-07 10:28:33 -0600

    Completed Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("Enter your grade: ");
+        string grade = Console.ReadLine();
+
+        int gradeNumber = int.Parse(grade);
+
+        string letter = "Blank";
+
+        if (gradeNumber >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeNumber >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradeNumber >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradeNumber >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your letter grade is {letter}");
+
+        if (gradeNumber >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed your course!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass your course. Better look next time!");
+        }
+       
     }
 }
