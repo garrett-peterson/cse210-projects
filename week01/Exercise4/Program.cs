33c889d40b90de0753f1a7a8b04d43fd4dcfb2e3
garrett-peterson <garrett.peterson726@gmail.com>
2026-01-07 11:53:08 -0600

    Finished exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of number, type 0 when finished.");
+        int notZero = 1;
+        List <int> numbers = new List<int>();
+
+
+        while (notZero != 0)
+        {
+            Console.Write("Enter number: ");
+            string answer = Console.ReadLine();
+            int numb = int.Parse(answer);
+
+            if (numb == 0)
+            {
+                notZero = 0;
+                int sum = numbers.Sum();
+                double avg = numbers.Average();
+                int large = numbers.Max();
+                int small = numbers.Where(n => n > 0).Min();
+
+                Console.WriteLine($"The sum is: {sum}");
+                Console.WriteLine($"The average is: {avg}");
+                Console.WriteLine($"The largest number is: {large}");
+                Console.WriteLine($"The smallest positive number is: {small}");
+
+            }
+
+            else
+            {
+                numbers.Add(numb);
+            }
+            
+        }
     }
 }
