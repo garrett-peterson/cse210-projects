afad219984869a6771e247407b40d8b79131f8f8
garrett-peterson <garrett.peterson726@gmail.com>
2026-01-21 10:48:40 -0600

    Completed the fraction project

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction fraction = new Fraction(3,4);
+
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
     }
 }
