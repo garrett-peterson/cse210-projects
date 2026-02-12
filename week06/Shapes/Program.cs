8ec57c25ec0f74996391cfe63ec017bccc0dd2e6
garrett-peterson <garrett.peterson726@gmail.com>
2026-02-12 14:41:34 -0600

    Did the shape program

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        Shape square = new Square(5, "Red");
+        Shape rect = new Rectangle(4,6,"Blue");
+        Shape circle = new Circle(3, "Green");
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(square);
+        shapes.Add(rect);
+        shapes.Add(circle);
+
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine(shape.GetColor());
+        }
     }
 }
